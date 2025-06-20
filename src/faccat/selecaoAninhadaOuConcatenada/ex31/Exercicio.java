e33fcf32092e4ea77810b9ac926113d28f10ca71
Rafael Rios <106401143+r-riosp@users.noreply.github.com>
2025-06-20 20:47:05 -0300

    Corrigido Exercicio 31

--- src/faccat/selecaoAninhadaOuConcatenada/ex31/Exercicio.java
+++ src/faccat/selecaoAninhadaOuConcatenada/ex31/Exercicio.java
@@ -3,11 +3,21 @@ package faccat.selecaoAninhadaOuConcatenada.ex31;
 import java.util.Scanner;
 
 public class Exercicio {
+    // Ler três valores inteiros e verificar se eles podem ser os lados de um triângulo.
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Código do exercício 31
-
-        sc.close();
+        System.out.println("Verificador de lados de triângulo");
+        System.out.print("Digite o lado A: ");
+        int ladoA = sc.nextInt();
+        System.out.print("Digite o lado B: ");
+        int ladoB = sc.nextInt();
+        System.out.print("Digite o lado C: ");
+        int ladoC = sc.nextInt();
+        
+        if (ladoA + ladoB > ladoC && ladoA + ladoC > ladoB && ladoB + ladoC > ladoA) {
+            System.out.println("Os valores podem ser os lados de um triângulo.");
+        } else {
+            System.out.println("Os valores não podem ser os lados de um triângulo.");
+        }
     }
 }
