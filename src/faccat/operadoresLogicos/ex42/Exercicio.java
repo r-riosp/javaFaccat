93c669f4968d8651156161684093f578f0b08a05
r-riosp <rafaelrios807@gmail.com>
2025-06-20 21:40:45 -0300

    Finalizando a lista de exercícios da apostila Faccat.

--- src/faccat/operadoresLogicos/ex42/Exercicio.java
+++ src/faccat/operadoresLogicos/ex42/Exercicio.java
@@ -1,4 +1,4 @@
-package faccat.ex42;
+package faccat.operadoresLogicos.ex42;
 
 import java.util.Scanner;
 
@@ -7,7 +7,21 @@ public class Exercicio {
         Scanner sc = new Scanner(System.in);
 
         // Código do exercício 42
-
+        System.out.println("Digite o código do empregado: ");
+        int codigo = sc.nextInt();
+        System.out.println("Digite o ano de nascimento do empregado: ");
+        int anoNascimento = sc.nextInt();
+        System.out.println("Digite o ano de contratação do empregado: ");
+        int anoContratacao = sc.nextInt();
+        System.out.println("Digite o ano atual: ");
+        int anoAtual = sc.nextInt();
         sc.close();
+
+        int idade = anoAtual - anoNascimento;
+        int tempoServico = anoAtual - anoContratacao;
+        boolean podeAposentar = (idade >= 65) || (tempoServico >= 30) || (idade >= 60 && tempoServico >= 25);
+
+        System.out.printf("Código do Empregado: %d%nIdade = %d%nTempo de serviço = %d%nPode se aposentar? %s%n",
+                codigo, idade, tempoServico, podeAposentar ? "Sim" : "Não");
     }
 }
--- src/faccat/operadoresLogicos/ex43/Exercicio.java
+++ src/faccat/operadoresLogicos/ex43/Exercicio.java
@@ -1,13 +1,36 @@
-package faccat.ex43;
+package faccat.operadoresLogicos.ex43;
 
 import java.util.Scanner;
+
+/**
+ * Exemplos de saída do exercício:
+ * <ul>
+ *   <li>A = 1, B = 2, C = 3 &rarr; Não foi possível formar um triângulo.</li>
+ *   <li>A = 3, B = 4, C = 5 &rarr; Triângulo escaleno.</li>
+ *   <li>A = 2, B = 2, C = 4 &rarr; Não foi possível formar um triângulo.</li>
+ *   <li>A = 4, B = 4, C = 4 &rarr; Triângulo equilátero.</li>
+ *   <li>A = 5, B = 3, C = 3 &rarr; Triângulo isósceles.</li>
+ * </ul>
+ */
 
 public class Exercicio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Código do exercício 43
-
+        System.out.println("Digite três valores separados por um espaço: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         sc.close();
+        if ((a < b + c) && (b < a + c) && (c < a + b)) {
+            if (a == b && b == c) {
+                System.out.println("Triângulo equilátero.");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Triângulo isósceles.");
+            } else {
+                System.out.println("Triângulo escaleno.");
+            }
+        } else {
+            System.out.println("Não foi possível formar um triângulo.");
+        }
     }
 }
